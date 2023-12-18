eeeb5ee3776ccb658c4a026fdf6328dd495f9fc5
AyushLochan <139963233+AyushLochan@users.noreply.github.com>
2023-12-18 19:25:10 +0530

    Third Commit
    Remaining operations

---
+++ storage/framework/views/719104e9739b8f2eb4748247fc40d88f8991e1f4.php
@@ -0,0 +1,197 @@
+
+
+<?php $__env->startSection('content'); ?>
+
+
+        <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Jared Bot</title>
+    <meta name="author" content="Xmartlabs" />
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+
+    <link href="https://fonts.googleapis.com/css?family=Amaranth|Roboto" rel="stylesheet">
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+    <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+
+    <link href="css/custom.css" rel="stylesheet" type="text/css" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Jared-Bot" />
+    <meta property="og:url" content="https://jaredbot.com" />
+    <meta property="og:image" content="https://d18tndtkm7kj2p.cloudfront.net/jared/jared-illustration.png" />
+    <meta property="og:title" content="Jared-Bot" />
+    <meta property="og:description" content="Update your Android project dependencies instantly and automatically. Trust Jared and he'll deliver." />
+
+    <meta property="fb:app_id" content="1895239457410968" />
+  </head>
+
+  <body>
+    
+
+    <div class="container terms-and-conditions">
+      <h1>Terms and Conditions</h1>
+      of Xmartlabs SRL (Xmartlabs) regarding the software Jared as of 04/2017.
+
+      <div class="row">
+        <div class="col col-xs-12">
+          1. Subject matter
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            1. The subject matter of this Agreement is the use of Jared. This Agreement regulates all relations between Jared and the customer regarding the use of Jared.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            2. Jared communicates with the GitHub hosting service which is offered by GitHub Inc. Condition to the proper use of Jared is a valid contract with GitHub Inc. on the use of GitHub which may lead to costs on the customer’s sole responsibility. The customer will allow Jared to access the GitHub repositories the customer chooses. The customer is solely liable for any costs or damages that GitHub Inc. associates with this communication.
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-xs-12">
+          2. Scope of services
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            1. Xmartlabs provides Jared as a service. This means, that Jared may be used via the internet only.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            2. Jared is a hosted service that provides the infrastructure for updating software projects dependencies. The functional range of the service is dependant on the package the customer chooses.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            3. Jared is integrated with GitHub and offers support for Android repositories.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            4. No consultancy, training, trouble shooting or support is within the scope of the services offered by Jared under this Agreement.
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-xs-12">
+          3. Obligations of the customer
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            1. The customer must not interfere or intent to interfere in any manner with the functionality or proper working of Jared.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            2. The customer will indemnify and hold harmless Jared, its officers and directors, employees and agents from any and all third party claims, damages, costs and (including reasonable attorneys fees) arising out of the customer’s use of Jared in a manner not authorized by this Agreement, and/or applicable law, or the customer’s or it’s employees’ or personnel’s negligence or willful misconduct.
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-xs-12">
+          4. Downtime and services suspensions
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            1. Adjustments, changes and updates of Jared that help to avoid or maintain dysfunctions of the software may lead to temporary service suspensions. Jared will try to limit downtime of the service or restrictions of accessibility to 10 hours a month. Jared will try to do regular maintenance works during the weekend or at times between 10 p.m. and 6 a.m. (CET).
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            2. The customer is aware that the service relies on a working internet infrastructure. Additional downtime of the service can occur, if the website is not available and at any other time with restrictive access to the internet.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            3. The customer is aware that Jared does not work if GitHub is not properly available (be it to Jared or the customer).
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-xs-12">
+          5. Rights to use
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            1. The customer is granted a limited, non-exclusive, non-transferable, non-sublicenseable right to use Jared as software as a service via the internet.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            2. The customer is not granted any additional right to the Software or any other intellectual property of Jared. This especially means that the customer shall not be entitled to make copies of the Software. The customer shall not translate the program code into other forms of code (decompilation) or employ other methods aimed at revealing the Software’s code in the various stages of its development (reverse engineering).
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            3. The customer is not entitled to remove or make alterations to copyright notices, serial numbers or other features which serve to identify the Software.
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col col-xs-12">
+          6. Warranty
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            1. Defects in the supplied Software shall be remediated within a reasonable time following a detailed notification of such defect being given to Jared by the customer.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            2. For the purpose of remedying defects, Jared may choose to replace the defective Software with a version of the Software which is free of defects.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            3. Unless Jared fails to repair or replace the Software, the right of the customer to terminate the contract due to an inability to use the Software shall be excluded.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            4. The limitation period for all warranty claims shall be 12 months commencing with the first coming to show of the defect.
+          </div>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col col-xs-12">
+          7. Liability
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            1. The pre-contractual, contractual and non-contractual liability of Jared is limited to cases of intent and gross negligence.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            2. In cases of ordinary negligence Jared’s liability shall be limited to the breach of contractual obligations which are Cardinal Obligations. Cardinal Obligations are those obligations for which due fulfillment is essential to the proper implementation of the contract as a whole, and the contractual partner may depend upon their fulfillment.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            3. Furthermore, the amount of damages shall be limited to those losses which are generally foreseeable in connection with the supply of the Software.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            4. The above limitations of liability shall also apply where Jared is vicariously responsible for its employees and agents.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            5. The limitations of liability in the section 7 shall not apply to liability resulting from injury to the life, body or health of a person or under the German Product Liability Law.
+          </div>
+          <div class="col col-xs-11 col-xs-offset-1 col-sm-11 col-sm-offset-1">
+            6. Any claims for damages by the customer relating to defects in the supplied Software shall be subject to a limitation period of 12 months. This does not apply to defects which have been intentionally concealed by Jared.
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        
+
+    <footer class="footer">
+      <div class="row">
+        <div class="col col-sm-4 col-xs-12 text-center">
+          <a href="https://xmartlabs.com" target="_blank">
+            <img class="footer-logo" src="images/xmartlabs-logo-grey.png" srcset="images/xmartlabs-logo-grey@2x.png 2x, images/xmartlabs-logo-grey@3x.png 3x" />
+          </a>
+        </div>
+
+        <div class="col col-sm-3 col-sm-offset-1 col-xs-12">
+          <p class="footer-text">Check out our amazing <a class="footer-link" href="https://github.com/xmartlabs" target="_blank">Github projects</a> for more!</p>
+        </div>
+
+        <div class="col col-sm-3 col-sm-offset-1 col-xs-12">
+          <p class="footer-text">Copyright Xmartlabs 2017</p>
+        </div>
+      </div>
+    </footer>
+    
+  </body>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-29494874-11', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+
+  <script
+    src="https://code.jquery.com/jquery-3.1.1.min.js"
+    integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+    crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<html>
+
+    <?php $__env->stopSection(); ?>       
+<?php echo $__env->make('l1.l1', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Project-4\resources\views///terms.blade.php ENDPATH**/ ?>
